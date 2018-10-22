8ccc78f75597d5e3fcb38211b803ff0e7557bd07
Rye-Guy <ryanracioppo95@gmail.com>
2018-10-22 19:50:35 -0400

    set up project and rendered a book list :D

---
+++ README.md
@@ -0,0 +1,7 @@
+# ReduxSimpleStarter
+
+Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+
+
+#### Learning Exercise
+This project was created alongside react-redux course
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Moby Dick'},
+        {title: 'A Clockwork Orange'},
+        {title: 'The Old Man and the Sea'},
+        {title: 'Lord of The Rings'}
+    ]
+}
